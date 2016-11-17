144261e0cf7fc58532eee0305a8cc7afa2f52616
adam2326 <adampilz.2326@gmail.com>
2016-11-17 12:05:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import logging
-
+from random import randint
 from flask import Flask
 
 
@@ -11,6 +11,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello Adams New World!'
 
+@app.route('/random')
+def myrando():
+	return '<h1>{}</h1>'.format(randint(0,9))
 
 @app.errorhandler(500)
 def server_error(e):
